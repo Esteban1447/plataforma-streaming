3191b9a6d707e4236694affa75c879ae912c1b63
Esteban1447 <esteban.robledo.rc@gmail.com>
2025-06-11 11:47:51 -0500

    Cambiar Login.css a loginStyles.css

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { registerUser } from '../services/auth';
 import Swal from 'sweetalert2';
-import '../styles/Login.css';
+import '<div styleName={} />
+<styles />
+<loginStyles></loginStyles>.css';
 
 function Register() {
   const navigate = useNavigate();
